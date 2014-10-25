21e4a0accbbd618d3e73b51e9a5048fd987e8156
sub77 <sub77@ymail.com>
2014-10-25 23:09:08 +0200

    init

--- init/Android.mk
+++ init/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
-LOCAL_SRC_FILES := init_ms013g.c
+LOCAL_SRC_FILES := init_matissewifi.c
 LOCAL_MODULE := libinit_msm
 include $(BUILD_STATIC_LIBRARY)
 
