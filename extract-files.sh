a5fdc4a5af69696d479f99195598d2589d67503f
sub77 <sub77@ymail.com>
2014-10-25 23:02:16 +0200

    del

--- extract-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-export VENDOR=samsung
-export DEVICE=matissewifi
-./../../samsung/msm8226-common/extract-files.sh $@
--- setup-makefiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-export DEVICE=matissewifi
-export VENDOR=samsung
-./../../$VENDOR/msm8226-common/setup-makefiles.sh $@
